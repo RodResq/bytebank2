3cd2ec9e28c9888698fa1a6e43477513309a5c54
rodrigo.resque <rodrigo.resque@tre-pa.jus.br>
2021-01-11 23:29:40 -0300

    updt

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,56 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height: 120,
+        theme: ThemeData(
+            primaryColor: Colors.green[900],
+            accentColor: Colors.blueAccent[700],
+            buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary,
+            )),
+        home: Dashboard());
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png')),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 80,
               width: 100,
-              color: Colors.green,
+              color: Theme.of(context).primaryColor,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.people),
-                  Text('Contatcts')
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contatcts',
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  )
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
